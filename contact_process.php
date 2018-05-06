b8367d9cd6df9dd527538c5154c787c25f340e67
usamabhatti159 <usamaMAC@Usamas-Macbook-Pro.local>
2018-05-06 17:03:42 +0500

    edit

--- contact_process.php
+++ contact_process.php
@@ -12,7 +12,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Ramirez.";
+    $subject = "You have a message from" .$subject;
 
     $logo = 'http://srthemes.com/demo/ramirez/image/logo.png';
     $link = '#';
@@ -33,4 +33,15 @@
 
     $send = mail($to, $subject, $body, $headers);
 
+
+
+
+
+
 ?>
+
+
+
+
+
+
